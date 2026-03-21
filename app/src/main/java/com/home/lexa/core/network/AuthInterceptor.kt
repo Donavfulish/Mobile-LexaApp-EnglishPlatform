88aea292ecafb2c5478fa578efbdcf74f517c0e6
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-21 13:58:28 +0700

    feat: add authinterceptor

---
+++ app/src/main/java/com/home/lexa/core/network/AuthInterceptor.kt
@@ -0,0 +1,22 @@
+// File: app/src/main/java/com/home/lexa/core/network/AuthInterceptor.kt
+package com.home.lexa.core.network
+
+import com.home.lexa.data.local.TokenManager
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val tokenManager: TokenManager) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+
+        // Lấy token từ local storage
+        val token = tokenManager.getToken()
+
+        // Nếu có token, gắn vào header
+        if (!token.isNullOrEmpty()) {
+            requestBuilder.addHeader("Authorization", "Bearer $token")
+        }
+
+        return chain.proceed(requestBuilder.build())
+    }
+}
