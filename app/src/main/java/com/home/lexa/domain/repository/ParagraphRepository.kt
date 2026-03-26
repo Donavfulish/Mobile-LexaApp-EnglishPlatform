6543beb6262f51a13bac2457816421aef4d6e9e7
Donavfulish <dvha23@clc.fitus.edu.vn>
2026-03-26 15:32:33 +0700

    feat(repo): add repo to update para result

--- app/src/main/java/com/home/lexa/domain/repository/ParagraphRepository.kt
+++ app/src/main/java/com/home/lexa/domain/repository/ParagraphRepository.kt
@@ -2,10 +2,13 @@ package com.home.lexa.domain.repository
 
 import com.home.lexa.domain.models.CreateParagraphRequest
 import com.home.lexa.domain.models.ParagraphResponseDto
+import com.home.lexa.domain.models.ParagraphResultResponseDto
 import com.home.lexa.domain.models.UpdateParagraphRequest
+import com.home.lexa.domain.models.UpdateParagraphResultRequest
 
 interface ParagraphRepository {
     suspend fun createParagraph(request: CreateParagraphRequest): Result<ParagraphResponseDto>
     suspend fun updateParagraph(paragraphId: Long, request: UpdateParagraphRequest): Result<ParagraphResponseDto>
     suspend fun deleteParagraph(paragraphId: Long): Result<Unit>
+    suspend fun updateParagraphResult(request: UpdateParagraphResultRequest): Result<ParagraphResultResponseDto>
 }
