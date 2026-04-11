b1db480b36ac527e684aa9beb2c8fa29051d6dab
minhtrile293 <23127273@student.hcmus.edu.vn>
2026-04-11 22:23:25 +0700

    feat: pagination logic for student course list

--- app/src/main/java/com/home/lexa/ui/course/student_course_list/StudentCourseListFragment.kt
+++ app/src/main/java/com/home/lexa/ui/course/student_course_list/StudentCourseListFragment.kt
@@ -3,33 +3,31 @@ package com.home.lexa.ui.course.student_course_list
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import androidx.navigation.fragment.findNavController
-import com.home.lexa.databinding.FragmentStudentCourseListBinding
-import com.home.lexa.R
-
-
-
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
+import com.home.lexa.R
 import com.home.lexa.core.base.BaseFragment
+import com.home.lexa.databinding.FragmentStudentCourseListBinding
+import com.home.lexa.domain.models.SearchInfo
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import kotlin.getValue
 
 class StudentCourseListFragment : BaseFragment<FragmentStudentCourseListBinding>(FragmentStudentCourseListBinding::inflate) {
     private val viewModel: StudentCourseListModel by viewModel()
     private val courseAdapter by lazy {
-        StudentCourseListAdapter(emptyList())
-        { course ->
+        StudentCourseListAdapter(emptyList()) { course ->
             val bundle = Bundle().apply {
                 putLong("courseId", course.id)
             }
             findNavController().navigate(R.id.courseDetailFragment, bundle)
+        }.apply {
+            stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
         }
     }
 
     private fun updateFilterUI(filter: CourseFilter) {
-
         fun setActive(btn: MaterialButton) {
             btn.setBackgroundResource(R.drawable.bg_filter_active)
             btn.backgroundTintList = null
@@ -55,11 +53,12 @@ class StudentCourseListFragment : BaseFragment<FragmentStudentCourseListBinding>
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        viewModel.fetchAllCourses();
-    }
     override fun setupViews() {
+        binding.headerSection.setHeaderData(
+            title = "Khoá học của tôi",
+            actionText = "0 tất cả",
+            onActionClick = {}
+        )
 
         val filterArg = arguments?.getString("filter")
         val initialFilter = try {
@@ -68,45 +67,138 @@ class StudentCourseListFragment : BaseFragment<FragmentStudentCourseListBinding>
             CourseFilter.ALL
         }
 
-        viewModel.changeFilter(initialFilter)
+        viewModel.changeFilter(initialFilter, SearchInfo(
+            query= "",
+            sortBy= "",
+            order= "",
+            limit = 10
+        ),
+            null)
 
         binding.rvCourses.apply {
             adapter = courseAdapter
             layoutManager = LinearLayoutManager(context)
         }
 
+
         binding.btnAll.setOnClickListener {
-            viewModel.changeFilter(CourseFilter.ALL)
+            if(viewModel.currentFilter.value == CourseFilter.ALL)
+                return@setOnClickListener
+            binding.headerSection.setHeaderData(
+                title = "Khoá học của tôi",
+                actionText = "0 tất cả",
+                onActionClick = {}
+            )
+            viewModel.changeFilter(
+                CourseFilter.ALL,
+                SearchInfo(
+                    query= "",
+                    sortBy= "",
+                    order= "",
+                    limit = 10
+                ),
+                null)
         }
 
         binding.btnFavorite.setOnClickListener {
-            viewModel.changeFilter(CourseFilter.FAVORITE)
+            if(viewModel.currentFilter.value == CourseFilter.FAVORITE)
+                return@setOnClickListener
+            binding.headerSection.setHeaderData(
+                title = "Khoá học của tôi",
+                actionText = "0 tất cả",
+                onActionClick = {}
+            )
+            viewModel.changeFilter(CourseFilter.FAVORITE,
+                SearchInfo(
+                    query= "",
+                    sortBy= "",
+                    order= "",
+                    limit = 10
+                ),
+                null)
         }
 
         binding.btnLearning.setOnClickListener {
-            viewModel.changeFilter(CourseFilter.LEARNING)
+            if(viewModel.currentFilter.value == CourseFilter.LEARNING)
+                return@setOnClickListener
+            binding.headerSection.setHeaderData(
+                title = "Khoá học của tôi",
+                actionText = "0 tất cả",
+                onActionClick = {}
+            )
+            viewModel.changeFilter(CourseFilter.LEARNING,
+                SearchInfo(
+                    query= "",
+                    sortBy= "",
+                    order= "",
+                    limit = 10
+                ),
+                null)
         }
 
-        viewModel.fetchAllCourses()
+        if (viewModel.courses.value.isNullOrEmpty()) {
+            viewModel.fetchAllCourses(
+                isLoadMore = false,
+                searchInfo = SearchInfo(query = "", limit = 10),
+                nextCursor = null
+            )
+        }
+
+        val layoutManager = LinearLayoutManager(context)
+        binding.rvCourses.apply {
+            this.layoutManager = layoutManager
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    if (dy > 0) {
+                        val visibleItemCount = layoutManager.childCount
+                        val totalItemCount = layoutManager.itemCount
+                        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                        val threshold = 3
+                        if (viewModel.isLoading.value == false && !viewModel.isLastPage) {
+                            if ((visibleItemCount + firstVisibleItemPosition) >= (totalItemCount - threshold)) {
+                                viewModel.fetchAllCourses(
+                                    isLoadMore = true,
+                                    searchInfo = SearchInfo(limit = 10),
+                                    nextCursor = viewModel.lastId
+                                )
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.fetchAllCourses(
+            isLoadMore = false,
+            searchInfo = SearchInfo(query = "", limit = 10),
+            nextCursor = null
+        )
     }
 
     override fun observeData() {
+        viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        }
+
         viewModel.courses.observe(viewLifecycleOwner) { list ->
             courseAdapter.updateData(list)
 
             val filter = viewModel.currentFilter.value ?: CourseFilter.ALL
-
             val title = when (filter) {
                 CourseFilter.ALL -> "Tất cả khoá học"
                 CourseFilter.FAVORITE -> "Khoá học yêu thích"
                 CourseFilter.LEARNING -> "Khoá học đang học"
             }
 
-
-
             binding.headerSection.setHeaderData(
                 title = title,
-                actionText = "${list.size} tất cả",
+                actionText = "${viewModel.totalPages} tất cả",
                 onActionClick = {}
             )
         }
--- app/src/main/java/com/home/lexa/ui/course/student_course_list/StudentCourseListModel.kt
+++ app/src/main/java/com/home/lexa/ui/course/student_course_list/StudentCourseListModel.kt
@@ -1,9 +1,11 @@
 package com.home.lexa.ui.course.student_course_list
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.home.lexa.domain.models.SearchInfo
 import com.home.lexa.domain.models.ShortCourseDto
 import com.home.lexa.domain.repository.CourseRepository
 import kotlinx.coroutines.launch
@@ -16,33 +18,59 @@ enum class CourseFilter {
 
 class StudentCourseListModel(private val repository: CourseRepository) : ViewModel() {
 
-    lateinit var filter: CourseFilter
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
     private val _currentFilter = MutableLiveData(CourseFilter.ALL)
     val currentFilter: LiveData<CourseFilter> = _currentFilter
-    // LiveData để chứa danh sách Decks
+
     private val _courses = MutableLiveData<List<ShortCourseDto>>(emptyList())
     val courses: LiveData<List<ShortCourseDto>> get() = _courses
 
-    fun changeFilter(filter: CourseFilter) {
+    var lastId: Long? = null
+    var isLastPage = false
+    var currentPages = 0
+    var totalPages = 0
+
+    fun changeFilter(filter: CourseFilter, searchInfo: SearchInfo, nextCursor: Long?) {
+        Log.d("File filter", filter.toString())
         if (_currentFilter.value == filter) return
         _currentFilter.value = filter
-        fetchAllCourses()
+
+        fetchAllCourses(false, searchInfo, nextCursor)
     }
-    fun fetchAllCourses() {
+
+    fun fetchAllCourses(isLoadMore: Boolean, searchInfo: SearchInfo, nextCursor: Long?) {
+        if (isLoadMore && (isLastPage || _isLoading.value == true)) return
+
+        if (!isLoadMore) {
+            isLastPage = false
+            lastId = null
+            currentPages = 0
+            _courses.value = emptyList()
+        }
+
         viewModelScope.launch {
             _isLoading.value = true
 
             val result = when (_currentFilter.value) {
-                CourseFilter.ALL -> repository.getFavoriteDecks()
-                CourseFilter.FAVORITE -> repository.getFavoriteDecks()
-                CourseFilter.LEARNING -> repository.getLearningCourses()
+                CourseFilter.ALL -> repository.getAllCourses(searchInfo, nextCursor)
+                CourseFilter.FAVORITE -> repository.getFavoriteCourses(searchInfo, nextCursor)
+                CourseFilter.LEARNING -> repository.getLearningCourses(searchInfo, nextCursor)
+                else -> repository.getAllCourses(searchInfo, nextCursor)
             }
 
             result.onSuccess { list ->
-                _courses.value = list
+                currentPages += list.data.size
+                totalPages = list.totalItem.toInt()
+                lastId = list.nextCursor
+
+                _courses.value = list.data
+
+                if(currentPages.toLong() == list.totalItem){
+                    isLastPage = true
+                    lastId = null
+                }
             }.onFailure {
                 _courses.value = emptyList()
             }
